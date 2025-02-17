483b6200b7d2a25fd657f7fe7a7a67faa63e3bcc
antonin.visnak <antonin.visnak@portabo.cz>
2025-02-17 17:06:53 +0100

    getr

--- APR2/tridy.py
+++ APR2/tridy.py
@@ -1,19 +1,29 @@
+import random
+
 class Pes:
     def __init__(self, jmeno, zvuk):
         # po REALIZACI třídy - jako konstruktor
-        self.jmeno = jmeno
+        self._jmeno = jmeno
         self.zvuk = zvuk
         pass
 
-    def stekej(self):
+    @property
+    def jmeno(self):
+        return "fuck off hafhaf"
+        # if random.choice["reknu", "nereknu"] == "nereknu":
+        #     return "haf co je ti do toho haf"
+        # else:
+        #     return self._jmeno
+
+    def stekej(self) -> None:
         print(f"Pes {self.jmeno} steka {self.zvuk}")
 
-    def curej(self, nakoho: str):
+    def curej(self, nakoho: str) -> None:
         print(f"Pes {self.jmeno} čůrá na {nakoho}")
 
 
 p1 = Pes("Azor", "haf haf")
-p1.stekej()
-p2 = Pes("Jonatán", "haf vrrr haf vrrr")
-p2.stekej()
-    
+# p1.stekej()
+# p2 = Pes("Jonatán", "haf vrrr haf vrrr")
+# p2.stekej()
+print(p1.jmeno)
