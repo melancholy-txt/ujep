ce4c0884a24c7706eaf9d6875f1e8dee70ddd46d
antonin.visnak <antonin.visnak@portabo.cz>
2025-02-24 16:51:33 +0100

    fronta

--- APR2/abstraktni_datove_struktury.py
+++ APR2/abstraktni_datove_struktury.py
@@ -1,5 +1,45 @@
-class Fronta:
+class Queue:
     def __init__(self):
-        self.data = []
+        self._data = []
+
+    def enque(self, item) -> None:
+        self._data.append(item)
+
+    def dequeue(self) -> int:
+        if not self._data or self.size() == 0:
+            raise IndexError("dequeue from an empty queue")
+        return self._data.pop(0)
+    
+    def front(self):
+        return self._data[0]
+    
+    def rear(self):
+        return self._data[-1]
+    
+    def isEmpty(self) -> bool:
+        return False if self.size() > 0 else True
+    
+    def size(self) -> int:
+        return len(self._data)
+    
+fronta = Queue()
+fronta.enque(1)
+fronta.enque(2)
+fronta.enque(3)
+print(fronta._data)
+print(fronta.dequeue())
+print(fronta.dequeue())
+print(fronta.size())
+print(fronta._data)
+print(fronta.isEmpty())
+print(fronta.dequeue())
+print(fronta.isEmpty())
+print(fronta.size())
+print(fronta.dequeue())
+
+
+
+
+
 
     
