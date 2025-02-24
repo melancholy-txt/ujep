ba984eebd39079d96f0b168e608a2f4766d9955e
antonin.visnak <antonin.visnak@portabo.cz>
2025-02-24 17:01:41 +0100

    zasobnik

--- APR2/abstraktni_datove_struktury.py
+++ APR2/abstraktni_datove_struktury.py
@@ -21,21 +21,56 @@ class Queue:
     
     def size(self) -> int:
         return len(self._data)
+
+
+class Stack:
+    def __init__(self):
+        self._data = []
+
+    def push(self, item) -> None:
+        self._data.append(item)
+
+    def pop(self) -> int:
+        if not self._data or self.size() == 0:
+            raise IndexError("dequeue from an empty queue")
+        return self._data.pop(-1)
     
-fronta = Queue()
-fronta.enque(1)
-fronta.enque(2)
-fronta.enque(3)
-print(fronta._data)
-print(fronta.dequeue())
-print(fronta.dequeue())
-print(fronta.size())
-print(fronta._data)
-print(fronta.isEmpty())
-print(fronta.dequeue())
-print(fronta.isEmpty())
-print(fronta.size())
-print(fronta.dequeue())
+    def peek(self) -> int:
+        return self._data[-1]
+    
+    def isEmpty(self) -> bool:
+        return False if self.size() > 0 else True
+    
+    def size(self) -> int:
+        return len(self._data)
+    
+
+
+
+zasob = Stack()
+zasob.push(1)
+zasob.push(2)
+zasob.push(3)
+print(zasob._data)
+print(zasob.peek())
+print(zasob.pop())
+print(zasob.pop())
+print(zasob.pop())
+
+# fronta = Queue()
+# fronta.enque(1)
+# fronta.enque(2)
+# fronta.enque(3)
+# print(fronta._data)
+# print(fronta.dequeue())
+# print(fronta.dequeue())
+# print(fronta.size())
+# print(fronta._data)
+# print(fronta.isEmpty())
+# print(fronta.dequeue())
+# print(fronta.isEmpty())
+# print(fronta.size())
+# print(fronta.dequeue())
 
 
 
