deed93cccdf609dedab4e59c2c976423e637e6ec
antonin.visnak <antonin.visnak@portabo.cz>
2025-03-10 16:50:32 +0100

    refactor bubblesort for improved clarity and efficiency

--- APR2/sekvencni_trideni.py
+++ APR2/sekvencni_trideni.py
@@ -1,21 +1,25 @@
 def bubblesort(f: list, asc=True) -> list:
-    swapped = True
-    while swapped:
-        swapped = False
-        for i in range(1, len(f)):
+    issorted = False
+    irazeni = 0
+    while not issorted:
+        issorted = True
+        for i in range(1, len(f) - irazeni):
             if f[i-1] > f[i]:
                 f[i-1], f[i] = f[i], f[i-1]
-                swapped = True
-        if not swapped:
-            if asc:
-                return f
-            else:
-                return f[::-1]
+                issorted = False
+        irazeni += 1
+        # if not issorted:
+        #     if asc:
+        #         return f
+        #     else:
+        #         return f[::-1]
     if asc:
         return f
     else:
         return f[::-1]
 
+def selectsort(f: list, asc=True) -> list:
+    
 
 f1 = [4,2,3,1]
 f2 = [4,6,3,1,9,7,5,3]
