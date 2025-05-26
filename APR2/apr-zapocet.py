f79582f9bc983414d382ca87bf3d31c52664c06a
Antonín Višňák <tovisnak@gmail.com>
2025-05-26 12:43:52 +0200

    dfs

--- APR2/apr-zapocet.py
+++ APR2/apr-zapocet.py
@@ -1,5 +1,5 @@
 #import diktyonphi as dp
-from diktyonphi import Graph, GraphType
+from diktyonphi import Graph, GraphType, Node
 
 # Create a directed graph
 g = Graph(GraphType.UNDIRECTED)
@@ -9,12 +9,23 @@ g.add_node("A", {"label": "Start", "color": "green"})
 g.add_node("B", {"label": "Middle", "color": "yellow"})
 g.add_node("C", {"label": "End", "color": "red"})
 g.add_node("D", {"label": "Optional", "color": "blue"})
+g.add_node("E", {"label": "Optional", "color": "blue"})
+g.add_node("F", {"label": "Optional", "color": "blue"})
+g.add_node("G", {"label": "Optional", "color": "blue"})
+g.add_node("H", {"label": "Optional", "color": "blue"})
 
 # Add edges with attributes
 g.add_edge("A", "B", {"weight": 1.0, "type": "normal"})
 g.add_edge("B", "C", {"weight": 2.5, "type": "critical"})
 g.add_edge("A", "D", {"weight": 0.8, "type": "optional"})
 g.add_edge("D", "C", {"weight": 1.7, "type": "fallback"})
+g.add_edge("D", "E", {"weight": 1.7, "type": "fallback"})
+g.add_edge("E", "C", {"weight": 1.7, "type": "fallback"})
+g.add_edge("F", "C", {"weight": 1.7, "type": "fallback"})
+g.add_edge("F", "G", {"weight": 1.7, "type": "fallback"})
+g.add_edge("F", "B", {"weight": 1.7, "type": "fallback"})
+g.add_edge("F", "H", {"weight": 1.7, "type": "fallback"})
+g.add_edge("E", "H", {"weight": 1.7, "type": "fallback"})
 
 def nodes_and_neighours(g: Graph, weights: bool = False ):   
     for node_id in g.node_ids():
@@ -38,7 +49,7 @@ def adjacency_matrix(g: Graph):
     for i, node_id in enumerate(g.node_ids()):
         # print(i, " ", node_id)
         node = g.node(node_id)
-        if node.out_degree == 0:
+        if g.type == GraphType.UNDIRECTED and node.out_degree == 0:
             continue
         # print(i, " ", node_id)
         for j, dest_id in enumerate(g.node_ids()):
@@ -50,6 +61,34 @@ def adjacency_matrix(g: Graph):
                 # matrix[i][j] = "matrix[i][j] + 1"
     for row in matrix:
         print(row, "\n")
+    
+def dfs(g: Graph, node_id, dest_id, visited: set = None) -> set:
+    if visited == None:
+        visited = set()
+
+    if node_id in visited:
+        return []
+
+    visited.add(node_id)   
+    path = [node_id]
+
+    if node_id == dest_id:
+        return path
+    
+    node = g.node(node_id)
+    for neigh_id in node.neighbor_ids:
+        if neigh_id not in visited:
+            neigh_dfs = dfs(g, neigh_id, dest_id, visited)
+            if neigh_dfs and dest_id in neigh_dfs:
+                return path + ["->"] + neigh_dfs 
+    return []
+        
+
+def bfs(g: Graph, node_id, dest_id):
+    queue = [node_id]
+
+    while queue:
+        current = queue.pop(0)
     ...
 
 print("-----------------")
@@ -58,3 +97,6 @@ print("-----------------")
 nodes_and_neighours(g, True)
 print("-----------------")
 adjacency_matrix(g)
+print("-----------------")
+print(g.to_dot())
+print(dfs(g, "A", "H"))
