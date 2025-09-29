e365c2e5c583e3f2e72a059b680781f824943c87
melancholy-txtt <tovisnak@gmail.com>
2025-09-29 11:36:40 +0200

    w -> omega

--- PZS/seminar1.py
+++ PZS/seminar1.py
@@ -3,20 +3,22 @@ import matplotlib.pyplot as plt
 
 A0 = 1
 f = 1
-W = 2 * np.pi * f
+omega = 2 * np.pi * f
 
 M = 10
 fmod = 10
 phi = 2 * np.pi * fmod
 
+
+
 def func(t):
-    return A0 * np.sin(W * t)
+    return A0 * np.sin(omega * t)
 
 def m(t):
     return M * np.sin(phi * t)
 
 def modulated(t):
-    return ( A0 + m(t) ) * np.sin(W * t)
+    return ( A0 + m(t) ) * np.sin(omega * t)
 
 t = np.linspace(0, 5, 1000)
 plt.figure(figsize=(10, 6))
@@ -28,4 +30,4 @@ plt.xlabel('t (s)')
 plt.ylabel('Amplituda')
 plt.legend()
 plt.grid()
-plt.show()      
+plt.shoomega()      
