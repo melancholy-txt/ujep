1d8ba30f9c8751e97d48cbd4cde50260c48f14d1
melancholy-txtt <tovisnak@gmail.com>
2025-09-30 09:42:46 +0200

    interface, myysql+mongo classes

--- OONV/FactoryMethod/Program.cs
+++ OONV/FactoryMethod/Program.cs
@@ -1,5 +1,35 @@
 ﻿namespace FactoryMethod;
 
+interface Database
+{
+    public string ReadRecord();
+    public string WriteRecord();
+}
+
+class MySQL : Database
+{
+    public MySQL() { }
+    public string ReadRecord() { return "find(...)"; }
+    public string WriteRecord() { return "insertOne(...)"; }
+
+}
+
+class MongoDB : Database
+{
+    public MongoDB() { }
+    public string ReadRecord() { return "select * from db"; }
+    public string WriteRecord() { return "insert '' into ..."; }
+
+}
+
+class FactoryMethod
+{
+    public Database CreateProduct(string typ)
+    {
+
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
