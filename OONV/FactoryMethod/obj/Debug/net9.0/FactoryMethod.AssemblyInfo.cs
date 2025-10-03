419d82ac1948d821b235343acadbd6d7e602f5c5
melancholy-txtt <tovisnak@gmail.com>
2025-10-03 10:05:58 +0200

    added a portforward to rdbs docker-compose

--- OONV/FactoryMethod/obj/Debug/net9.0/FactoryMethod.AssemblyInfo.cs
+++ OONV/FactoryMethod/obj/Debug/net9.0/FactoryMethod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactoryMethod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb774df40749be759a9546a4180e4b7e6837fcbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23beb6fa4486e15865964d3abb63f117e5160eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoryMethod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactoryMethod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
