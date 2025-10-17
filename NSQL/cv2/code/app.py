0f8163eb9d8e6a6f343e5edebb5281df73ed21ec
melancholy-txtt <tovisnak@gmail.com>
2025-10-17 11:50:21 +0200

    lol xd jsem flask god

--- NSQL/cv2/code/app.py
+++ NSQL/cv2/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for   
 
 app = Flask(__name__)
 
@@ -34,9 +34,22 @@ def zobraz_home():
 def zobraz_kocky():
     return render_template("kocky.html", data=kocky)
 
-@app.route("/kontakt")
+@app.route("/kontakt", methods=["GET", "POST"])
 def zobraz_kontaktni_formular():
-    return render_template("kontakt.html")
+    if request.method == "GET":
+        return render_template("kontakt.html")
+    elif request.method == "POST":
+        jmeno = request.form.get("jmeno")
+        barva = request.form.get("srst")
+        vek = request.form.get("vek")
+        nova_kocka = {
+            "jmeno": jmeno,
+            "barva": barva,
+            "vek": int(vek),    
+        }
+        kocky.append(nova_kocka)
+        return redirect(url_for("zobraz_kocky"))    
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
