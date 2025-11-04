dfab1b7d39c0e0e5144768ecd47f0f8e648dbf71
Antonin Visnak <tovisnak@gmail.com>
2025-11-04 09:03:41 +0100

    adapter method

---
+++ OONV/AdapterMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+
+namespace AdapterMethod;
+
+interface INotifier
+{
+    void Send(string message);
+}
+
+class EmailNotifier : INotifier
+{
+    private readonly LegacyEmail _emailService;
+
+    public EmailNotifier(LegacyEmail emailService)
+    {
+        _emailService = emailService;
+    }
+
+    public void Send(string message)
+    {
+        _emailService.SendEmail("from@example.com", "to@example.com", message);
+    }
+}
+
+class LegacyEmail
+{
+    public void SendEmail(string from, string to, string message)
+    {
+        Console.WriteLine($"Email sent from: {from}, to: {to}, with message: {message}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LegacyEmail legacyEmail = new LegacyEmail();
+        INotifier emailNotifier = new EmailNotifier(legacyEmail);
+        emailNotifier.Send("Hello, this is a test email!");
+    }
+}
