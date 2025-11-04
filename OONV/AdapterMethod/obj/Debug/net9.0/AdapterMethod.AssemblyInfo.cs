940d2bf4fa4fe413523bb868c7d1f29368c6042b
Antonin Visnak <tovisnak@gmail.com>
2025-11-04 09:44:50 +0100

    update assembly information and source link version

--- OONV/AdapterMethod/obj/Debug/net9.0/AdapterMethod.AssemblyInfo.cs
+++ OONV/AdapterMethod/obj/Debug/net9.0/AdapterMethod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdapterMethod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfab1b7d39c0e0e5144768ecd47f0f8e648dbf71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d4cf566c6dcdecb863fbf1cd10627c3859b89a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdapterMethod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdapterMethod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
