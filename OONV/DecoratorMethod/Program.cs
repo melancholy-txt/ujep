8c7ff83ee398478af0d9c49cd7710db93002b7fd
Antonin Visnak <tovisnak@gmail.com>
2025-11-18 08:53:39 +0100

    v1 of Decorator

--- OONV/DecoratorMethod/Program.cs
+++ OONV/DecoratorMethod/Program.cs
@@ -1,4 +1,36 @@
-﻿namespace DecoratorMethod;
+﻿using System.ComponentModel.Design;
+
+namespace DecoratorMethod;
+
+public interface ICoffeeMaker
+{
+    public string BrewCoffee();
+}
+
+class BasicCoffeeMaker() : ICoffeeMaker
+{
+    public string BrewCoffee()
+    {
+        return "Černá káva co vychladne";
+    }
+}
+
+abstract class BaseDecorator(ICoffeeMaker coffeeMaker) : ICoffeeMaker
+{
+    public virtual string BrewCoffee()
+    {
+        return coffeeMaker.BrewCoffee();
+    }
+}
+
+class MilkMachine(ICoffeeMaker coffeeMaker) : BaseDecorator(coffeeMaker)
+{
+    public override string BrewCoffee()
+    {
+        return base.BrewCoffee() + " s mlékem";
+    }
+    
+}
 
 class Program
 {
