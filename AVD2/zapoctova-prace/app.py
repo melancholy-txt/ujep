27b8e3d56b66a7d8922b68ccf08b59d79b244545
Antonin Visnak <tovisnak@gmail.com>
2026-04-21 10:59:32 +0200

    data baby

--- AVD2/zapoctova-prace/app.py
+++ AVD2/zapoctova-prace/app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    data = pd.read_csv("data/DOD_2026_data.csv")
+    return data
+
+df = load_data()
+
 
 st.title("My uv-managed Streamlit App 🚀")
 st.write("If you are reading this, uv and Streamlit are working perfectly together!")
 
-# A quick interactive widget to test
-name = st.text_input("What's your name?")
-if name:
-    st.success(f"Hello, {name}!")
+df
